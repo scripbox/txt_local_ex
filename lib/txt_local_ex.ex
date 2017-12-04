1595f7c791916a89e4f2994a0a41a83bcc543210
Nitin Misra <nitin.misra23@gmail.com>
2017-12-04 15:12:10 +0530

    Initial Commit

---
+++ lib/txt_local_ex.ex
@@ -0,0 +1,18 @@
+defmodule TxtLocalEx do
+  @moduledoc """
+  Documentation for TxtLocalEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TxtLocalEx.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/txt_local_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule TxtLocalExTest do
+  use ExUnit.Case
+  doctest TxtLocalEx
+
+  test "greets the world" do
+    assert TxtLocalEx.hello() == :world
+  end
+end
