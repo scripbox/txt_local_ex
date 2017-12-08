c6a59393185a4f0c8796426a99cd3f58abfbb413
Nitin Misra <nitin.misra23@gmail.com>
2017-12-08 17:57:16 +0530

    Add custom exception ApiLimitExceeded

--- lib/errors.ex
+++ lib/errors.ex
@@ -7,3 +7,13 @@ defmodule TxtLocalEx.Errors.ApiError do
   def message(%__MODULE__{reason: reason}),
     do: "TxtLocalEx::ApiError - #{reason}"
 end
+
+defmodule TxtLocalEx.Errors.ApiLimitExceeded do
+  defexception [:reason]
+
+  def exception(reason),
+    do: %__MODULE__{reason: reason}
+
+  def message(%__MODULE__{reason: reason}),
+    do: "TxtLocalEx::ApiLimitExceeded - #{reason}"
+end
