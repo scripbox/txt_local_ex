1652ddbc10aed5cda139092d7a90807a9ab6d174
Nitin Misra <nitin.misra23@gmail.com>
2018-07-25 22:24:16 +0530

    Fix HTTPoison.Base warnings

--- lib/request.ex
+++ lib/request.ex
@@ -3,7 +3,7 @@ defmodule TxtLocalEx.Request do
 
   @api_url "http://api.textlocal.in"
 
-  defp process_url(url) do
+  def process_url(url) do
     @api_url <> url
   end
 
@@ -11,15 +11,15 @@ defmodule TxtLocalEx.Request do
     %{"Content-Type" => "application/x-www-form-urlencoded"}
   end
 
-  defp process_request_body(body) when is_map(body) do
+  def process_request_body(body) when is_map(body) do
     body
     |> URI.encode_query()
   end
 
-  defp process_request_body(body), do: body
+  def process_request_body(body), do: body
 
   # Override the base headers with any passed in.
-  defp process_request_headers(request_headers) do
+  def process_request_headers(request_headers) do
     headers =
       request_headers
       |> Enum.into(%{})
@@ -30,7 +30,7 @@ defmodule TxtLocalEx.Request do
 
   # :timeout - timeout to establish a connection, in milliseconds.
   # :recv_timeout - timeout used when receiving a connection.
-  defp process_request_options(_options) do
+  def process_request_options(_options) do
     [timeout: 5000, recv_timeout: 2000]
   end
 
